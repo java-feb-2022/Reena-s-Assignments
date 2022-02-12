469d75c96b2dd7884a8e49acccf5b20f29608e48
reenadangi <reena.dangi@gmail.com>
2022-02-12 12:10:18 -0600

    Algo session

---
+++ Week1/Extra Practice/Algo.java
@@ -0,0 +1,11 @@
+public class Algo {
+    public static void main(String[] args) {
+        // Create an array - 1-10
+        int[] numbers={1,2,3,4,5,6,7,8,9,10};
+        
+        // replace all evens with 0
+        // Print the array - 1,0,3,0,....
+
+    }
+    
+}
